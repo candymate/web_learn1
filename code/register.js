117d55da977727080842c078730b9f05ff07dfcc
candymate <ys3f4@naver.com>
2019-04-30 18:19:26 +0900

    Added handler of register fail

--- code/register.js
+++ code/register.js
@@ -11,7 +11,10 @@ module.exports = (router) => {
     User.create({
       userID: req.body.id,
       password_hash: crypto.createHash("sha256").update(req.body.password).digest("hex")
+    }).then(function (item) {
+      res.send("Success<br><a href=\"/\">Back to main</a>");
+    }).catch(function (err) {
+      res.send("Error<br><a href=\"/register\">Back to register</a>");
     });
-    res.send("Success<br><a href=\"/\">Back to main</a>");
   });
 }
